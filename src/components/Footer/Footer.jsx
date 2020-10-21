3a6935c1dac752e1537bc8e44c124ab903252727
agusp-dev <perezp.agustin@gmail.com>
2020-10-21 12:05:55 -0300

    Fix issue in footer responsive.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { BottomNavigation, Grid, Typography } from "@material-ui/core";
+import { BottomNavigation, Grid, Typography, Hidden } from "@material-ui/core";
 
 import { useStyles } from "./style";
 
@@ -15,38 +15,86 @@ const Footer = () => {
         container
         className={classes.NavAll}
       >
-        <Grid item xs={12} sm={3} style={{ textAlign: 'start' }}>
-          <Scale />
-        </Grid>
-        <Grid
-          item
-          xs={12} sm={6}
-          style={{ textAlign: 'center', color: '#fff' }}
-        >
-          <Typography style={{ fontSize: '15px' }}>
-            <a
-              className={classes.links}
-              href="http://me-elecmetal.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ME Elecmetal
-            </a>{" "}
-            © Todos los derechos reservados. Powered by{" "}
-            <a
-              className={classes.links}
-              href="https://wisely.cl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Wisely
-            </a>
-            .
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={3} style={{ textAlign: 'end' }}>
-          <MultiLanguage />
-        </Grid>
+        <Hidden xsDown>
+          <Grid item sm={3} style={{ textAlign: 'start' }}>
+            <Scale />
+          </Grid>
+          <Grid
+            item
+            sm={6}
+            style={{ textAlign: 'center', color: '#fff' }}
+          >
+            <Typography style={{ fontSize: '15px' }}>
+              <a
+                className={classes.links}
+                href="http://me-elecmetal.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ME Elecmetal
+              </a>{" "}
+              © Todos los derechos reservados. Powered by{" "}
+              <a
+                className={classes.links}
+                href="https://wisely.cl"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Wisely
+              </a>
+              .
+            </Typography>
+          </Grid>
+          <Grid item sm={3} style={{ textAlign: 'end' }}>
+            <MultiLanguage />
+          </Grid>
+        </Hidden>
+        <Hidden only={[ 'sm', 'md', 'lg', 'xl' ]}>
+          <Grid
+            item
+            xs={12}
+            style={{ 
+              textAlign: 'center',
+              color: '#fff', 
+              marginBottom: '16px',
+              display: 'flex',
+              flexDirection: 'row',
+            }}>
+              <Grid item xs={6}>
+                <Scale />
+              </Grid>
+
+              <Grid item xs={6}>
+                <MultiLanguage />
+              </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            style={{ textAlign: 'center', color: '#fff' }}
+          >
+            <Typography style={{ fontSize: '15px' }}>
+              <a
+                className={classes.links}
+                href="http://me-elecmetal.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ME Elecmetal
+              </a>{" "}
+              © Todos los derechos reservados. Powered by{" "}
+              <a
+                className={classes.links}
+                href="https://wisely.cl"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Wisely
+              </a>
+              .
+            </Typography>
+          </Grid>
+        </Hidden>
       </Grid>
     </BottomNavigation>
   );
