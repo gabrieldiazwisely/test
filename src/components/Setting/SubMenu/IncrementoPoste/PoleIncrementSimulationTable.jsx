d355f443c312b3abf1cc031a800b3c87a6a09e3a
Cesar Augusto Krampanis <ckrampanis@vates.com>
2020-11-27 11:22:26 -0300

    removed comment

--- src/components/Setting/SubMenu/IncrementoPoste/PoleIncrementSimulationTable.jsx
+++ src/components/Setting/SubMenu/IncrementoPoste/PoleIncrementSimulationTable.jsx
@@ -50,7 +50,6 @@ const PoleIncrementSimulationTable = ({ settingPoleIncrementData }) => {
   return  (
     <Grid item xs={12} sm={6}>
       <Paper elevation={3}>
-        cesar
         <Box p={2}>
           <Box  className={classes.boxRight}>
             <FormControl variant="outlined" className={classes.formControl} >
